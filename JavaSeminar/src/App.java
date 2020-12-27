e78fba1f31c7ac184be3922be6e6e74c19066330
Alexander Frewein <alexander.frewein@student.tugraz.at>
2020-12-27 09:34:35 +0100

    upload java project the first time

---
+++ JavaSeminar/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int variable_a = 40;
+        int variable_b = 50;
+        int x = 5;
+        int y = 10;
+        x = x + y;
+        variable_a = x - variable_b;
+        System.out.println("Result: " + x);
+        System.out.println("Result2: " + variable_a);
+        variable_a = 0;
+
+
+    
+    }
+}
